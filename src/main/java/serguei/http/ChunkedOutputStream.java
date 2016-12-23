f9b643f622c5bd91a43164aac310ae9ab6716d42
Serguei Poliakov <spoliako@cisco.com>
2016-12-23 20:26:39 +0000

    Created ChunkedOutputStream

---
+++ src/main/java/serguei/http/ChunkedOutputStream.java
@@ -0,0 +1,78 @@
+package serguei.http;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ChunkedOutputStream extends OutputStream {
+
+    private static final String CODEPAGE = "ASCII";
+    private static final byte[] CRLF = {13, 10};
+    private static final byte[] LAST_CHUNK = {'0', 13, 10, 13, 10};
+    private static final int DEFAULT_CHUNK_SIZE = 16384;
+
+    private final OutputStream outputStream;
+    private final byte[] chunkBuffer;
+    private int bytesInChunk = 0;
+
+    public ChunkedOutputStream(OutputStream outputStream) {
+        this(outputStream, DEFAULT_CHUNK_SIZE);
+    }
+
+    public ChunkedOutputStream(OutputStream outputStream, int chunkSize) {
+        this.outputStream = outputStream;
+        this.chunkBuffer = new byte[chunkSize];
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        if (bytesInChunk >= chunkBuffer.length) {
+            writeChunk();
+        }
+        chunkBuffer[bytesInChunk++] = (byte)b;
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        if (b == null) {
+            throw new NullPointerException();
+        } else if (off < 0 || off > b.length || len < 0 || off + len > b.length || off + len < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (len == 0) {
+            return;
+        }
+        int left = len;
+        while (left > chunkBuffer.length - bytesInChunk) {
+            System.arraycopy(b, off + len - left, chunkBuffer, bytesInChunk, chunkBuffer.length - bytesInChunk);
+            left -= chunkBuffer.length - bytesInChunk;
+            bytesInChunk = chunkBuffer.length;
+            writeChunk();
+        }
+        if (left > 0) {
+            System.arraycopy(b, off + len - left, chunkBuffer, bytesInChunk, left);
+            bytesInChunk = left;
+        }
+    }
+
+    @Override
+    public void flush() throws IOException {
+        outputStream.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (bytesInChunk > 0) {
+            writeChunk();
+        }
+        outputStream.write(LAST_CHUNK);
+        outputStream.close();
+    }
+
+    private void writeChunk() throws IOException {
+        outputStream.write(Integer.toHexString(bytesInChunk).getBytes(CODEPAGE));
+        outputStream.write(CRLF);
+        outputStream.write(chunkBuffer, 0, bytesInChunk);
+        outputStream.write(CRLF);
+        bytesInChunk = 0;
+    }
+
+}
---
+++ src/test/java/serguei/http/ChunkedOutputStreamTest.java
@@ -0,0 +1,59 @@
+package serguei.http;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class ChunkedOutputStreamTest {
+
+    private byte[] OUTPUT_DATA = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10};
+    private byte[] EXPECTED = {'a', '\r', '\n', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, '\r', '\n', 'a', '\r', '\n', -1, -2,
+            -3, -4, -5, -6, -7, -8, -9, -10, '\r', '\n', '0', '\r', '\n', '\r', '\n'};
+
+    @Test
+    public void shouldWriteChunk() throws Exception {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        ChunkedOutputStream outputStream = new ChunkedOutputStream(output, 10);
+
+        outputStream.write(OUTPUT_DATA);
+        outputStream.close();
+
+        assertArrayEquals(EXPECTED, output.toByteArray());
+    }
+
+    @Test
+    public void shouldWriteChunkByByte() throws Exception {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        ChunkedOutputStream outputStream = new ChunkedOutputStream(output, 10);
+
+        for (byte ch : OUTPUT_DATA) {
+            outputStream.write((int)ch);
+        }
+        outputStream.close();
+
+        assertArrayEquals(EXPECTED, output.toByteArray());
+    }
+
+    @Test
+    public void shouldBeReadByChunkedInputStream() throws Exception {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        ChunkedOutputStream outputStream = new ChunkedOutputStream(output, 12);
+
+        outputStream.write(OUTPUT_DATA);
+        outputStream.close();
+
+        InputStream inputStream = new ByteArrayInputStream(output.toByteArray());
+        InputStream chunkedInputStream = new ChunkedInputStream(inputStream);
+
+        byte[] buffer = new byte[OUTPUT_DATA.length];
+        int read = chunkedInputStream.read(buffer);
+        assertEquals(OUTPUT_DATA.length, read);
+        assertEquals(-1, chunkedInputStream.read());
+        assertArrayEquals(OUTPUT_DATA, buffer);
+        chunkedInputStream.close();
+    }
+}
