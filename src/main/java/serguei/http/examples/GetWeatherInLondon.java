f20e29c96ea373fe10d2505b0a6f8665161051f7
Serguei Poliakov <spoliako@cisco.com>
2017-01-10 16:57:31 +0000

    Add HttpClientConnection example

---
+++ src/main/java/serguei/http/examples/GetWeatherInLondon.java
@@ -0,0 +1,111 @@
+package serguei.http.examples;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.security.cert.X509Certificate;
+
+import serguei.http.HttpClientConnection;
+import serguei.http.HttpRequestHeaders;
+import serguei.http.HttpResponse;
+
+/**
+ * This is an example of using HttpClientConnection
+ * 
+ * This program makes request to a free weather API with a request for weather in London and then extract temperature
+ * from returned JSON. The temperature is output to console.
+ * 
+ * If we use HTTPS version of the API (defined by WEATHER_API_URL value), we also output some extra SSL parameters
+ * 
+ * I can't guarantee that this site will always work as I have no relation to it.
+ * 
+ * @author Serguei Poliakov
+ *
+ */
+public class GetWeatherInLondon {
+
+    // replace https with http if accessing non-secure HTTP:
+    private static final String WEATHER_API_URL = "https://weathers.co/api.php";
+    private static final String CITY = "London";
+    private static final int OK_STATUS = 200;
+
+    private final BufferedWriter output;
+
+    public GetWeatherInLondon(OutputStream output) {
+        this.output = new BufferedWriter(new OutputStreamWriter(output));
+    }
+
+    public static void main(String[] args) {
+        GetWeatherInLondon process = new GetWeatherInLondon(System.out);
+        try {
+            process.doTheJob();
+            System.out.println("Finished");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doTheJob() throws IOException {
+        URL url = new URL(WEATHER_API_URL);
+        boolean ssl = url.getProtocol().equals("https");
+        int port = ssl ? 443 : 80;
+        HttpClientConnection connection = new HttpClientConnection(url.getHost(), port);
+        try {
+            if (ssl) {
+                // the following will not validate if the certificate is valid, for validation use
+                // startHandshakeAndValidate
+                connection.startHandshake();
+                outputSslParameters(connection);
+            }
+            HttpRequestHeaders requestHeaders = HttpRequestHeaders.getRequest(WEATHER_API_URL + "?city=" + CITY);
+            HttpResponse response = connection.send(requestHeaders);
+            if (response.getStatusCode() == OK_STATUS) {
+                String responseBody = response.readBodyAsString();
+                String temperature = getTemperatureFromResponseBody(responseBody);
+                output.write("Temperature in " + CITY + " is " + temperature);
+                output.newLine();
+            } else {
+                output.write("Incorrect response status: " + response.getStatusCode() + " " + response.getReason());
+                output.newLine();
+            }
+            output.flush();
+        } finally {
+            connection.close();
+        }
+    }
+
+    private String getTemperatureFromResponseBody(String json) throws IOException {
+        // don't want to import any JSON parsing library - quick and dirty solution
+        String valueName = "\"temperature\"";
+        int pos = json.indexOf(valueName);
+        if (pos > 0) {
+            int start = json.indexOf('"', pos + valueName.length());
+            if (start > 0) {
+                start++;
+                int end = json.indexOf('"', start);
+                if (end > 0) {
+                    return json.substring(start, end);
+                }
+            }
+        }
+        throw new IOException("Wrong JSON received: " + json);
+    }
+
+    private void outputSslParameters(HttpClientConnection connection) throws IOException {
+        output.write("TLS version used: " + connection.getNegotiatedSslProtocol().toJdkString());
+        output.newLine();
+        output.write("Negotiated cipher suite: " + connection.getNegotiatedCipher());
+        output.newLine();
+        output.write("Received certificates:");
+        output.newLine();
+        for (X509Certificate certificate : connection.getSslCertificates()) {
+            output.write("  " + certificate.getSubjectDN().getName());
+            output.newLine();
+        }
+        output.write("-----------------------");
+        output.newLine();
+    }
+
+}
---
+++ src/test/java/serguei/http/examples/GetWeatherInLondonTest.java
@@ -0,0 +1,25 @@
+package serguei.http.examples;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+
+import org.junit.Test;
+
+public class GetWeatherInLondonTest {
+
+    private static final String RESULT_PREFIX = "Temperature in London is ";
+
+    @Test
+    public void shoudReturnWeatherInLondong() throws Exception {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        GetWeatherInLondon weather = new GetWeatherInLondon(output);
+
+        weather.doTheJob();
+
+        String result = output.toString();
+        assertTrue("Wrong response: " + result, result.contains(RESULT_PREFIX));
+        String value = result.substring(result.indexOf(RESULT_PREFIX) + RESULT_PREFIX.length());
+        assertNotNull(Double.valueOf(value)); // this will fail if the value is not a number
+    }
+}
